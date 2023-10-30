f12c9d72c5080232750b0bdb7e88a6c33a76bd36
Nathan Leiby <nathanleiby@gmail.com>
2023-10-29 23:04:23 -0700

    movement in a direction

--- Game.gd
+++ Game.gd
@@ -1,32 +1,35 @@
 extends Node2D
 
 @onready var _player := $Player
+@onready var _playerArrow := $Player/Arrow
 @onready var _relic := $Relic
 
 
-var playerPos := Vector2i(0,0)
-
-var relicPos := Vector2i(1,1)
+var playerPos := Vector2(0,0)
+var playerRotation := Vector2.UP
+var relicPos := Vector2(1,1)
 
 const TILE_SIZE = 32
 
 func _updatePositions():
 	_player.position = playerPos * TILE_SIZE
+	_playerArrow.rotation = playerRotation.angle()
 	_relic.position = relicPos * TILE_SIZE 
 	
 func _ready():
 	_updatePositions()
 	
 func _on_left_pressed():
-	playerPos.x -= 1
+	playerRotation = playerRotation.rotated(-PI/2)
 	_updatePositions()
 
 func _on_forward_pressed():
-	playerPos.y -= 1
+	print(rotation)
+	playerPos = playerPos + playerRotation
 	_updatePositions()
 
 func _on_right_pressed():
-	playerPos.x += 1
+	playerRotation = playerRotation.rotated(PI/2)
 	_updatePositions()
 
 
